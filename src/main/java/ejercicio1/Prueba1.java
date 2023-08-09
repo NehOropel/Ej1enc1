909d39986579a4e19fad74fee039b20eb08eb36a
NegusOro <negusoro07@gmail.com>
2023-08-09 18:06:55 -0300

    nuevo
    pryeba

---
+++ src/main/java/ejercicio1/Prueba1.java
@@ -0,0 +1,30 @@
+
+package ejercicio1;
+
+public class Prueba1 {
+    private String nombre;
+    private int edad;
+
+    public Prueba1(String nombre, int edad) {
+        this.nombre = nombre;
+        this.edad = edad;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+    
+    
+}
